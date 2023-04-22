b1ee0724fb12391a51f810de45ceacef5b235e00
Bryan W <bhoff1980@gmail.com>
2023-04-22 17:29:23 -0600

    added dark mode button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { logo } from "./assets";
 import { Home, CreatePost } from "./pages";
+import { Button } from "react-bootstrap";
 
 const App = () => {
+  const [dark, setDark] = useState(false)
+
+  const mode =() => {
+    if (dark === true){
+      setDark(false)
+    }else setDark(true)
+  }
+  console.log(dark)
   return (
     <BrowserRouter>
-      <header className="w-full flex justify-between items-center bg-white sm:px-8 py-4 border-b border-b-[#e6ebf4]">
+      <header className="w-full flex justify-between items-center sm:px-8 py-4 border-b">
         <Link to="/">
           <img src={logo} alt="logo" className="w-52 object-contain" />
         </Link>
-        <Link to="/create-post" className="font-inter font-medium bg-[#6449ff] text-white px-4 py-2 rounded-md">Create</Link>
+        <div>
+        <Link to="/create-post" className="font-inter font-medium bg-[#6449ff] text-white px-4 py-2 mr-2 rounded-md">Create</Link>
+      
+      <Button  className= { dark === false ?  "bg-black rounded-md text-white px-4 py-[6px] font-medium" :
+      "bg-white rounded-md text-black px-4 py-[6px] font-medium border"}
+       onClick={mode}> { dark === false ? 'Light Mode' : 'Dark Mode'}</Button>
+      </div>
       </header>
       <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
 
